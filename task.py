410045cd7090026e7398a70c85acc428351f95ac
Maxus94 <kmaxusk@gmail.com>
2025-04-01 21:20:27 +0300

    templates-created

--- task.py
+++ task.py
@@ -1,4 +1,29 @@
-import asyncio
+import asyncio, argparse
 
 from aiopath import AsyncPath
 from aioshutil import copyfile
+
+parser = argparse.ArgumentParser(description="Sort files in folders")
+parser.add_argument("indir", type=str, help="Initial directory with files")
+parser.add_argument("outdir", type=str, help="Target directory")
+args = parser.parse_args()
+print(args)
+
+indir_path = AsyncPath(args.indir)
+outdir_path = AsyncPath(args.outdir)
+
+
+async def read_folder(folder):
+    pass
+
+
+async def copy_file(file, folder):
+    pass
+
+
+async def main():
+    await read_folder(indir_path)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
